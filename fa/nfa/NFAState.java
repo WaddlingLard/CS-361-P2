766b3cd8a65e39024927b6793840e358368d3e70
Brian Wu <brianwu@u.boisestate.edu>
2024-10-23 19:08:44 -0600

    NFAState implemented!

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -2,10 +2,42 @@ package fa.nfa;
 
 import fa.State;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+
 public class NFAState extends State {
 
+    private Map<Character, Set<NFAState>> states;
     public NFAState(String name) {
         super(name);
+        this.states = new HashMap<>();
     }
+
+    /*
+
+        @param
+        @param
+        @return
+     */
+    public Set<NFAState> addTransition(NFAState location, Character sigma) {
+        Set<NFAState> currentStates = this.getNFATransition(sigma);
+        if (currentStates == null) { // Need to create new set
+            Set<NFAState> newSet = new HashSet<>();
+            newSet.add(location);
+            return states.put(sigma, newSet);
+        } else { // There is a set holding transitions currently
+            currentStates.add(location);
+            return states.put(sigma, currentStates);
+        }
+    }
+
+    /*
+
+        @param
+        @return
+     */
+    public Set<NFAState> getNFATransition(Character sigma) { return states.get(sigma); }
 
 }
